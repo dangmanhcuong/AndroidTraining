ab88c810282801811e53ef61383055cd4d8647d0
Cuongdm <cuongdangmanh@vccorp.vn>
2017-08-21 12:52:39 +0700

    update test

--- app/src/main/java/com/sdmcsoftware/androidtraining/MainActivity.java
+++ app/src/main/java/com/sdmcsoftware/androidtraining/MainActivity.java
@@ -2,12 +2,31 @@ package com.sdmcsoftware.androidtraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private View.OnClickListener clickListenner = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+           switch (view.getId()){
+               case R.id.btid:
+                   Toast.makeText(MainActivity.this,"click button",Toast.LENGTH_LONG).show();
+               break;
+               default: break;
+           }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btid);
+        button.setOnClickListener(clickListenner);
+
     }
+
+
 }
